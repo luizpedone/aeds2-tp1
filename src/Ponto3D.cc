6e437b1cf5fcace8806e3f965fad61f2a4f9fb70
Luiz Felipe Pedone <luizpedone@users.noreply.github.com>
2014-12-31 11:35:50 -0200

    Remove unnecessary spaces

--- src/Ponto3D.cc
+++ src/Ponto3D.cc
@@ -19,21 +19,21 @@
 using namespace std;
 
 Ponto3D::Ponto3D(){
-    this -> x_ = 0.0;
-    this -> y_ = 0.0;
-    this -> z_ = 0.0;
+    this->x_ = 0.0;
+    this->y_ = 0.0;
+    this->z_ = 0.0;
 }
 
 Ponto3D::Ponto3D(double x, double y, double z){
-    this -> x_ = x;
-    this -> y_ = y;
-    this -> z_ = z;
+    this->x_ = x;
+    this->y_ = y;
+    this->z_ = z;
 }
 
 Ponto3D::Ponto3D(Ponto3D &p){
-    this -> x_ = p.x_;
-    this -> y_ = p.y_;
-    this -> z_ = p.z_;
+    this->x_ = p.x_;
+    this->y_ = p.y_;
+    this->z_ = p.z_;
 }
 
 double& Ponto3D::x(){
@@ -49,21 +49,21 @@ double& Ponto3D::z(){
 }
 
 void Ponto3D::operator=(Ponto3D& p){
-    this -> x_ = p.x_;
-    this -> y_ = p.y_;
-    this -> z_ = p.z_;
+    this->x_ = p.x_;
+    this->y_ = p.y_;
+    this->z_ = p.z_;
 }
 
 void Ponto3D::Transladar(double dx, double dy, double dz){
-    this -> x_ = x_ + dx;
-    this -> y_ = y_ + dy;
-    this -> z_ = z_ + dz;
+    this->x_ = x_ + dx;
+    this->y_ = y_ + dy;
+    this->z_ = z_ + dz;
 }
 
 void Ponto3D::MudarEscala(double fx, double fy, double fz){
-    this -> x_ = x_ * fx;
-    this -> y_ = y_ * fy;
-    this -> z_ = z_ * fz;
+    this->x_ = x_ * fx;
+    this->y_ = y_ * fy;
+    this->z_ = z_ * fz;
 }
 
 void Ponto3D::Rotacionar(char e, double arg){
@@ -71,14 +71,14 @@ void Ponto3D::Rotacionar(char e, double arg){
     if(e == 'X'){
         aux[1] = y_*cos(arg)+z_*(sin(arg));
         aux[2] = y_*(-sin(arg))+z_*cos(arg);
-        this -> y_ = aux[1];
-        this -> z_ = aux[2];
+        this->y_ = aux[1];
+        this->z_ = aux[2];
     }
     if(e == 'Y'){
         aux[0] = x_*cos(arg)+z_*(-sin(arg));
         aux[2] = x_*sin(arg)+z_*(cos(arg));
-        this -> x_ = aux[0];
-        this -> z_ = aux[1];
+        this->x_ = aux[0];
+        this->z_ = aux[1];
     }
     if(e == 'Z'){
         aux[0] = x_*cos(arg)+y_*(-sin(arg));
